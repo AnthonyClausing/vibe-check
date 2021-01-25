37fbccd461a920f52787eea3a3ecb6ba42ca06d0
AnthonyClausing <AnthonyClausing@gmail.com>
2021-01-24 19:18:33 -0500

    redo html -> pug server renders pug template css and script included

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
+const path = require('path')
 const express = require('express');
 const app = express();
 
 const IP = process.env.IP || '0.0.0.0';
 const PORT = process.env.PORT || 8080;
-
 const searchLyrics = require('./searchLyrics');
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// app.use('*', (req, res) => {
+//   res.sendFile(path.join(__dirname,'views/index.html'))
+// })
 app.get('/search', (request, response) => {
   const listId = request.query.list;
   if(listId != null){
     searchLyrics("PLfe2uTiC7lz-xG0N50SVCmpJ9-MrW5VpU")
     .then(results => {
+      console.log(results)
         response.status(200);
         response.json(results);
     });
@@ -19,6 +26,6 @@ app.get('/search', (request, response) => {
   }
 });
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.status(200).render('index'));
 
 app.listen(PORT, IP);
