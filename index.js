1a0fa12595c86407f106d230707a6564a2a95d65
AnthonyClausing <AnthonyClausing@gmail.com>
2021-01-27 17:44:50 -0500

    modal logic, include in index, style update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let checkButton = document.getElementById('check')
 let input = document.getElementById('playlist-input')
+let modal = document.getElementById('modal')
+let modalCloseButton = document.querySelector('#modal-close>button')
 //get and sanitize input value
 //Disable after click or disappear with animation
 //some type of loading animation
@@ -19,4 +21,7 @@ checkButton.addEventListener('click', () => {
   }
   // fetch('http://localhost:8080/search?list=PLfe2uTiC7lz-xG0N50SVCmpJ9-MrW5VpU')
   // .then(res => console.log(res))
+})
+modalCloseButton.addEventListener('click', () => {
+  modal.style.display = modal.style.display === 'none' ? 'block' : 'none' 
 })
